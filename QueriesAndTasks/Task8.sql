e100d36899c59ec8345e2aa9d7748f60177adac3
Ola Alataiwi <olaabilal1@gmail.com>
2022-12-16 19:06:58 +0200

    Resolving Task 8 (Using CREATE OR ALTER)

--- QueriesAndTasks/Task8.sql
+++ QueriesAndTasks/Task8.sql
@@ -5,13 +5,12 @@
 --Enemies
 
 
-DROP VIEW IF EXISTS viewEpisodes
-GO
-CREATE VIEW viewEpisodes AS
+CREATE OR ALTER VIEW viewEpisodes 
+AS
 SELECT a.AuthorName,d.DoctorName, dbo.fnCompanions(e.EpisodeId) AS Companions, dbo.fnEnemies(e.EpisodeId) AS Enemies
 FROM tblEpisode e LEFT JOIN tblAuthor a ON(e.AuthorID=a.AuthorId)
-LEFT JOIN tblDoctor d ON(e.DoctorId=d.DoctorId);
-GO
+LEFT JOIN tblDoctor d ON(e.DoctorId=d.DoctorId)
 
+GO
 
 select * from viewEpisodes;
